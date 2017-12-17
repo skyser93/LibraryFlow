d970f0392a45aaa6236861b9a6917c6752a5cb12
Denis <butakov.denis@ya.ru>
2017-12-17 21:34:14 +0500

    Initial commit

---
+++ src/main/java/ru/javabegin/training/springlibrary/objects/LibraryFacade.java
@@ -0,0 +1,65 @@
+package ru.javabegin.training.springlibrary.objects;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import ru.javabegin.training.springlibrary.dao.interfaces.BookDAO;
+import ru.javabegin.training.springlibrary.entities.Author;
+import ru.javabegin.training.springlibrary.entities.Book;
+import ru.javabegin.training.springlibrary.entities.Genre;
+import ru.javabegin.training.springlibrary.enums.SearchType;
+
+import java.util.List;
+
+@Component("libraryFacade")
+@Scope("singleton")
+public class LibraryFacade {
+
+    private static final String FIELD_CONTENT = "content";
+
+
+    @Autowired
+    private BookDAO bookDAO;
+
+    @Autowired
+    private SearchCriteria searchCriteria;
+
+    private List<Book> books;
+
+
+    public List<Book> getBooks() {
+        if (books == null){
+            books = bookDAO.getBooks();
+        }
+        return books;
+    }
+
+    public void searchBooksByLetter() {
+        books = bookDAO.getBooks(searchCriteria.getLetter());
+    }
+
+    public void searchBooksByGenre() {
+        books = bookDAO.getBooks(searchCriteria.getGenre());
+    }
+
+    public void searchBooksByText() {
+
+        switch (searchCriteria.getSearchType()){
+            case TITLE:
+                books = bookDAO.getBooks(searchCriteria.getText());
+                break;
+            case AUTHOR:
+                books = bookDAO.getBooks(new Author(searchCriteria.getText()));
+                break;
+        }
+
+    }
+
+    public byte[] getContent(long id){
+        return (byte[])bookDAO.getFieldValue(id, FIELD_CONTENT);
+    }
+
+
+
+}
